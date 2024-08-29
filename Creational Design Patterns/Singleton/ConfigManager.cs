c114d32af83d5b1b5675d511e8253e5574666f70
m3hdi <mehdi.k1917@gmail.com>
2024-08-30 00:57:30 +0330

    Add Singleton Pattern

---
+++ Creational Design Patterns/Singleton/ConfigManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ConfigManager
+    {
+        private Dictionary<string, object> settings = new Dictionary<string, object>();
+        private static ConfigManager instance = new ConfigManager();
+                
+        private ConfigManager(){}
+
+        public static ConfigManager getInstance() 
+        {
+            return instance;
+        }
+
+        public void set(string key, object value) 
+        {
+            settings.Add(key, value);
+        }
+
+        public object get(string key)
+        {
+            return settings[key];
+        }
+         
+    }
+} 
---
+++ Creational Design Patterns/Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var manager = ConfigManager.getInstance();
+        manager.set("name", "Mosh");
+
+        var other = ConfigManager.getInstance();
+        Console.WriteLine(other.get("name"));
+    }
+}
